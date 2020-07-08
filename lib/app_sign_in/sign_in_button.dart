a2201c1a8674b26fe62a4ba4fe91bff52642fec2
mrpandey1 <pandeynishant12345@gmail.com>
2020-07-08 16:59:22 +0530

    created a reusable SignInButton

--- lib/app_sign_in/sign_in_button.dart
+++ lib/app_sign_in/sign_in_button.dart
@@ -1,1 +1,18 @@
+import 'package:My_First_Project/shared/custom_raised_button.dart';
+import 'package:flutter/material.dart';
 
+class SignInButton extends CustomRaisedButton {
+  SignInButton({
+    String text,
+    Color color,
+    Color textColor,
+    VoidCallback onPressed,
+  }) : super(
+          child: Text(
+            text,
+            style: TextStyle(color: textColor, fontSize: 15.0),
+          ),
+          color: color,
+          onPressed: onPressed,
+        );
+}
